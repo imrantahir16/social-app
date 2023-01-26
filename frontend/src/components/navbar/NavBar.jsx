d709f45bf7a69a0f9590ac6a41606a7ed6b92c01
imrantahir16 <imrantahir16@gmail.com>
2023-01-27 02:50:46 +0500

    Added navbar component

---
+++ frontend/src/components/navbar/NavBar.jsx
@@ -0,0 +1,47 @@
+import { Chat, Notifications, Search, Person } from "@mui/icons-material";
+import styles from "./NavBar.module.css";
+
+const NavBar = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.left}>
+        <span className={styles.logo}>Social App</span>
+      </div>
+      <div className={styles.center}>
+        <div className={styles.searchBar}>
+          <Search className={styles.searchIcon} />
+          <input
+            className={styles.input}
+            placeholder="Search friends or post"
+          />
+        </div>
+      </div>
+      <div className={styles.right}>
+        <div className={styles.Links}>
+          <a href="/" className={styles.link}>
+            Homepage
+          </a>
+          <a href="/" className={styles.link}>
+            Timeline
+          </a>
+        </div>
+        <div className={styles.icons}>
+          <div className={styles.icon}>
+            <Person />
+            <span className={styles.iconBadge}>1</span>
+          </div>
+          <div className={styles.icon}>
+            <Chat />
+            <span className={styles.iconBadge}>1</span>
+          </div>
+          <div className={styles.icon}>
+            <Notifications />
+            <span className={styles.iconBadge}>1</span>
+          </div>
+        </div>
+        <img src={"/assets/profiles/1.jpg"} alt="" className={styles.profile} />
+      </div>
+    </div>
+  );
+};
+export default NavBar;
---
+++ frontend/src/pages/home/Home.jsx
@@ -0,0 +1,10 @@
+import NavBar from "../../components/navbar/NavBar";
+
+const Home = () => {
+  return (
+    <div>
+      <NavBar />
+    </div>
+  );
+};
+export default Home;
