a18fa1fe32a28d9971b52b10ba8e3d3589057bd1
imrantahir16 <imrantahir16@gmail.com>
2023-02-13 01:07:31 +0500

    change base url from local to real

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// export const BASE_URL = "https://social-app-production-418c.up.railway.app/";
-export const BASE_URL = "http://localhost:8080/";
+export const BASE_URL = "https://social-app-production-418c.up.railway.app/";
+// export const BASE_URL = "http://localhost:8080/";
 
 export default axios.create({
   baseURL: BASE_URL,
